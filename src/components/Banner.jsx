97c32723fd677b460f69808a649eee6cce9d181b
Zuber Khan <zuberkhan034@gmail.com>
2023-01-21 15:34:44 +0530

    Implemented pagination integration in trending movies

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { MutatingDots } from "react-loader-spinner";
 import axios from "axios";
+
 function Banner() {
+
+  // State Management
   const [movie, setMovie] = useState({});
+
+  // API management
   useEffect(() => {
     axios
       .get(
         `${process.env.REACT_APP_GET_MOVIES}${process.env.REACT_APP_TMDB_API_KEY}`
       )
       .then((res) => {
-        const randomMovie = Math.floor(Math.random() * res.data.results.length);
-        setMovie(res.data.results[randomMovie]);
+        const movies = res.data.results;
+        const random = Math.floor(Math.random() * movies.length);
+        const movie = movies[random];
+        setMovie(movie);
       });
   }, []);
 
+    // Component Handler
   return (
     <>
       <div className="h-[40vh] md:h-[60vh] mt-20">
@@ -36,7 +44,7 @@ function Banner() {
             className={`bg-[url(${process.env.REACT_APP_BASE_IMG_URL_BANNER}${movie.backdrop_path})] h-[40vh] md:h-[60vh] text-white bg-cover bg-top flex items-end mt-20`}
           >
             <div
-              className={`text-xl md:text-3xl p-4 bg-gray-900  bg-opacity-50 w-full flex justify-center`}
+              className={`text-xl md:text-3xl p-4 bg-gray-700  bg-opacity-50 w-full flex justify-center backdrop-blur-md`}
             >
               {movie.title}
             </div>
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,29 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Pagination() {
-  let [page, setPage] = useState(1);
-  const next = () => {
-    setPage(page + 1);
-  };
-  const prev = () => {
-    page > 1 ? setPage(page - 1) : (page = 1);
-  };
+// Pagination component function with props | parameters
+function Pagination({propPrev,propPage, propNext}) {
 
+  // Component Handler
   return (
     <>
       <div className="w-full flex justify-center mb-8">
         <button
           className="p-2 border-2 border-blue-500 text-blue-500 border-r-0 rounded-l-xl"
-          onClick={prev}
+          onClick={propPrev}
         >
           Previous
         </button>
         <button className="p-2 border-2 border-blue-500 text-blue-500  bg-gray-300">
-          {page}
+          {propPage}
         </button>
         <button
           className="p-2 border-2 border-blue-500 text-blue-500 border-l-0 rounded-r-xl"
-          onClick={next}
+          onClick={propNext}
         >
           Next
         </button>
